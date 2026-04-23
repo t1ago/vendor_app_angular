4b2b6971d0282c5634b5ebbe9d46a52a511f2bdf
Tiago H. PIantavinha <tiago.henrip@hotmail.com>
2026-04-23 17:31:30 -0300

    Padronização dos testes

--- src/app/login/services/login-service.spec.ts
+++ src/app/login/services/login-service.spec.ts
@@ -1,16 +1,101 @@
 import { TestBed } from '@angular/core/testing';
 
 import { LoginService } from './login-service';
+import { provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { Mocked, vi } from 'vitest';
+import { AuthLoginService } from '@shared/services/auth-login-service';
+import { AuthUserService } from '@shared/services/auth-user-service';
+import { AuthStoreService } from '@shared/services/auth-store-service';
+import { IAuthLoginModel } from '@shared/interfaces/auth-login-model';
+import { firstValueFrom } from 'rxjs/internal/firstValueFrom';
+import { IAuthUserModel } from '@shared/interfaces/auth-user-model';
+import { of, throwError } from 'rxjs';
+import { IAuthTokenModel } from '@shared/interfaces/auth-token-model';
 
 describe('LoginService', () => {
-  let service: LoginService;
+    let service: LoginService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(LoginService);
-  });
+    const authLoginServiceStub: Partial<Mocked<AuthLoginService>> = {
+        login: vi.fn(),
+    };
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    const authUserServiceStub: Partial<Mocked<AuthUserService>> = {
+        user: vi.fn(),
+    };
+
+    const authStoreServiceStub: Partial<Mocked<AuthStoreService>> = {
+        isLogged: vi.fn(),
+    };
+
+    beforeEach(() => {
+        TestBed.resetTestingModule();
+
+        TestBed.configureTestingModule({
+            providers: [
+                LoginService,
+                provideHttpClient(),
+                provideHttpClientTesting(),
+                { provide: AuthLoginService, useValue: authLoginServiceStub },
+                { provide: AuthUserService, useValue: authUserServiceStub },
+                { provide: AuthStoreService, useValue: authStoreServiceStub },
+            ],
+        });
+
+        service = TestBed.inject(LoginService);
+    });
+
+    it('should be created', () => {
+        expect(service).toBeTruthy();
+    });
+
+    it('should successful login', async () => {
+        // GIVEN
+        const loginModelMock: IAuthLoginModel = {
+            email: 'Test@Test.com',
+            password: '123456',
+        };
+
+        const tokenModelMock: IAuthTokenModel = {
+            token: 'abc123',
+            expiresIn: 3600,
+        };
+
+        const userModelMock: IAuthUserModel = {
+            credentialId: 0,
+            credentialName: 'Test',
+            profileId: 0,
+            profileName: 'Adm Test',
+            profileIdentifier: 'ADMTEST',
+        };
+
+        authLoginServiceStub.login?.mockReturnValueOnce(of(tokenModelMock));
+        authUserServiceStub.user?.mockReturnValueOnce(of(userModelMock));
+        authStoreServiceStub.isLogged?.mockReturnValueOnce(true);
+
+        // WHEN
+        const loginPromise = firstValueFrom(service.login(loginModelMock));
+        const loginResult = await loginPromise;
+
+        // THEN
+        expect(true).toBe(loginResult);
+    });
+
+    it('should Error login', async () => {
+        // GIVEN
+        const loginModelMock: IAuthLoginModel = {
+            email: 'Test@Test.com',
+            password: '123456',
+        };
+
+        const errorMock = new Error('Error Login');
+
+        authLoginServiceStub.login?.mockReturnValueOnce(throwError(() => errorMock));
+
+        // WHEN
+        const loginPromise = firstValueFrom(service.login(loginModelMock));
+
+        // THEN
+        await expect(loginPromise).rejects.toThrow(errorMock);
+    });
 });
--- src/app/main/features/products/category/services/category-service.spec.ts
+++ src/app/main/features/products/category/services/category-service.spec.ts
@@ -9,14 +9,7 @@ import { firstValueFrom } from 'rxjs';
 describe('CategoryService', () => {
     let service: CategoryService;
     let httpMock: HttpTestingController;
-    const apiUrl = '/tiago/categoria';
-
-    const mockDtoResponse: { data: ICategoryDto[] } = {
-        data: [
-            { id: 1, nome: 'Eletrônicos' },
-            { id: 2, nome: 'Livros' },
-        ],
-    };
+    const apiUrl = 'http://localhost:3000/tiago/categoria';
 
     beforeEach(() => {
         TestBed.resetTestingModule();
@@ -38,90 +31,121 @@ describe('CategoryService', () => {
     });
 
     it('should fetch all categories and map them to ICategoryModel[]', async () => {
-        const mockResponse: { data: ICategoryDto[] } = {
+        // GIVEN
+        const categoryDtoMock: { data: ICategoryDto[] } = {
             data: [
                 { id: 1, nome: 'Electronics' },
                 { id: 2, nome: 'Books' },
             ],
         };
 
-        const requestPromise = firstValueFrom(service.getAll());
+        const gelAllPromise = firstValueFrom(service.getAll());
 
         const req = httpMock.expectOne(apiUrl);
-        req.flush(mockResponse);
 
-        const models = await requestPromise;
+        // WHEN
+        req.flush(categoryDtoMock);
 
+        const models = await gelAllPromise;
+
+        // THEN
         expect(req.request.method).toBe('GET');
         expect(models.length).toBe(2);
         expect(models[0].name).toBe('Electronics');
     });
 
     it('should fetch a single category by ID and map it to ICategoryModel', async () => {
-        const mockResponse = {
+        // GIVEN
+        const categoryDtoMock = {
             data: [{ id: 1, nome: 'Eletrônicos' }],
         };
         const categoryId = 1;
 
-        const requestPromise = firstValueFrom(service.getById(categoryId));
+        const getByIdPromise = firstValueFrom(service.getById(categoryId));
 
         const req = httpMock.expectOne(`${apiUrl}/${categoryId}`);
+
+        // WHEN
+        req.flush(categoryDtoMock);
+
+        const model = await getByIdPromise;
+
+        // THEN
         expect(req.request.method).toBe('GET');
-        req.flush(mockResponse);
-
-        const model = await requestPromise;
-
         expect(model.id).toBe(1);
         expect(model.name).toBe('Eletrônicos');
     });
 
     it('should POST to create a new category when id is missing', async () => {
+        // GIVEN
         const newCategory = { name: 'Casa' };
-        const mockResponse = { success: true };
 
-        const requestPromise = firstValueFrom(service.save(newCategory as any));
+        const categoryResult = { success: true };
+
+        const savePromise = firstValueFrom(service.save(newCategory as any));
 
         const req = httpMock.expectOne(apiUrl);
+
+        // WHEN
+        req.flush(categoryResult);
+
+        await savePromise;
+
+        // THEN
         expect(req.request.method).toBe('POST');
 
         expect(req.request.body).toEqual({ id: undefined, nome: 'Casa' });
-
-        req.flush(mockResponse);
-        await requestPromise;
     });
 
     it('should PUT to update an existing category when id is present', async () => {
+        // GIVEN
         const existingCategory = { id: 10, name: 'Ferramentas' };
-        const mockResponse = { success: true };
 
-        const requestPromise = firstValueFrom(service.save(existingCategory));
+        const categoryResult = { success: true };
+
+        const savePromise = firstValueFrom(service.save(existingCategory));
 
         const req = httpMock.expectOne(`${apiUrl}/${existingCategory.id}`);
-        expect(req.request.method).toBe('PUT');
-        expect(req.request.body).toEqual({ id: 10, nome: 'Ferramentas' });
 
-        req.flush(mockResponse);
-        await requestPromise;
+        // WHEN
+        req.flush(categoryResult);
+
+        await savePromise;
+
+        // THEN
+        expect(req.request.method).toBe('PUT');
+
+        expect(req.request.body).toEqual({ id: 10, nome: 'Ferramentas' });
     });
 
     it('should DELETE a category by ID', async () => {
+        // GIVEN
         const categoryId = 5;
-        const mockResponse = {};
 
-        const requestPromise = firstValueFrom(service.delete(categoryId));
+        const categoryResult = {};
+
+        const deletePromise = firstValueFrom(service.delete(categoryId));
 
         const req = httpMock.expectOne(`${apiUrl}/${categoryId}`);
-        expect(req.request.method).toBe('DELETE');
 
-        req.flush(mockResponse);
-        await requestPromise;
+        // WHEN
+        req.flush(categoryResult);
+
+        await deletePromise;
+
+        // THEN
+        expect(req.request.method).toBe('DELETE');
     });
 
-    it('should correctly map Model to DTO and vice-versa', () => {
-        const model = { id: 99, name: 'Teste' };
-        const dto = { id: 99, nome: 'Teste' };
+    it('should correctly map Model to DTO and DTO tp Model', () => {
+        // GIVEN
+        const model = { id: 99, name: 'Test' };
 
+        const dto = { id: 99, nome: 'Test' };
+
+        // WHEN - THEN
         expect(service.mapDto(model)).toEqual(dto);
+
         expect(service.mapModel(dto)).toEqual(model);
     });
 });
